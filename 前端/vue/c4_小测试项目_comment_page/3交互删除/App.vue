fdde3d052f4cd72c5a37be056559653bf8347347
chengrui <1287221322@qq.com>
2019-12-19 10:00:11 +0800

    测试项目交互删除

---
+++ 前端/vue/c4_小测试项目_comment_page/3交互删除/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <add :addComments="addComments"/>
+      <!--组件之间的通信，此时传递的是contents变量的值-->
+      <list :contents="comments"
+            :deleteComments="deleteComments"/>
+    </div>
+  </div>
+</template>
+<script>
+  import add from "./components/add";
+  import list from "./components/list";
+
+  export default {
+    data(){
+      return{
+        comments : [
+          {name:'aa',content:'i love vue'},
+          {name:'bb',content:'i love aa & vue'},
+          {name:'cc',content:'i love bb & vue'}
+        ]
+      }
+    },
+    components :{
+      add,
+      list
+    },
+    methods:{
+      //添加评论
+      //需要将方法传递给组件
+      addComments(comment){
+        this.comments.unshift(comment)
+      },
+      //删除执行数据元素
+      deleteComments(index){
+        this.comments.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style>
+  .logo{
+    width: 150px;
+    height: 150px;
+  }
+</style>
+
---
+++ 前端/vue/c4_小测试项目_comment_page/3交互删除/item.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <li class="list-group-item">
+        <div class="handle">
+          <a href="javascript:;" @click="deleteItem">删除</a>
+        </div>
+        <p class="user"><span >{{comment.name}}</span><span>说:</span></p>
+        <p class="centence">{{comment.content}}/{{index}}</p>
+      </li>
+
+    </div>
+</template>
+
+<script>
+    export default {
+      //生命接收comment
+      props:{//指定属性名和属性值的类型
+        comment:Object,
+        deleteComments:Function,
+        index:Number
+
+      },
+    //   data(){
+    //     return{
+    //       index:''
+    //     }
+    // },
+      methods:{
+        deleteItem() {//删除item
+          //确认删除
+          const  {comment} = this.comment
+          if (window.confirm('确定删除 ${comment.name} 的评论？')){
+
+            alert(this.index)
+            //点击事件执行
+            this.deleteComments(this.index)
+          }
+
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
