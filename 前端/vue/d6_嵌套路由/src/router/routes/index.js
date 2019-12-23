11cdd91f2dbf07816437706392b3958af4f2870f
chengrui <1287221322@qq.com>
2019-12-23 20:42:31 +0800

    嵌套路由

---
+++ 前端/vue/d6_嵌套路由/src/router/routes/index.js
@@ -0,0 +1,63 @@
+/*
+路由器模块
+ */
+//引入
+//用于创建路由器的构建函数
+import VueRouter from "vue-router";
+//引入组件
+import About from "../views/About";
+import Home from "../views/Home";
+//引入vue对象
+import Vue from 'vue'
+//使用对象
+Vue.use(VueRouter)
+//暴露默认的属性，可以使用任何属性名
+
+
+//引入嵌套路由组件
+import News from "../views/home/News";
+import Message from "../views/home/Message";
+
+export default new VueRouter({
+  // 多个配置项
+  //由于是多个使用数组进行配置
+  routes:[
+    {
+      path:'/about',
+      component :About
+    },
+    {
+      path:'/home',
+      component :Home
+    },
+    {
+      //根路径
+      path:'/',
+      redirect:'/about'
+    },
+
+    //=============
+    //配置嵌套路由
+    {
+      path:'/home',
+      component:Home,
+      children:[//嵌套路由会有很多使用数组
+        {
+          path:'/home/news',  // '/'永远代表根目录
+          component:News
+        },
+        {
+          path:'message',//简化写法
+          component:Message
+        },
+        //默认显示页面
+        {
+          path:"",
+          redirect:'/home/news'
+        }
+
+      ]
+    }
+  ]
+
+})
---
+++ 前端/vue/d6_嵌套路由/src/router/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h3>Home页面</h3>
+
+      <div>
+        <ul class="nav nav-tabs">
+          <li><router-link to="/home/news">News</router-link></li>
+          <li><router-link to="/home/message">Message</router-link></li>
+        </ul>
+
+        <router-view></router-view>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
